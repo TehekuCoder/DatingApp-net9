7f7d6e9d5c6b7378a22328d0b4816aae5967766b
TehekuCoder <j.d.w.w@gmx.de>
2024-12-16 17:18:25 +0100

    End of Section 8

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -17,21 +17,24 @@ public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
 {
 
     if (await UserExists(registerDto.Username)) return BadRequest("Username is taken!");
-    using var hmac = new HMACSHA512();
 
-    var user = new AppUser{
-        UserName = registerDto.Username.ToLower(),
-        PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-        PasswordSalt = hmac.Key
-    };
+    return Ok();
 
-    context.Users.Add(user);
-    await context.SaveChangesAsync();
+    // using var hmac = new HMACSHA512();
 
-    return new UserDto{
-        Username = user.UserName,
-        Token = tokenService.CreateToken(user)
-    };
+    // var user = new AppUser{
+    //     UserName = registerDto.Username.ToLower(),
+    //     PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+    //     PasswordSalt = hmac.Key
+    // };
+
+    // context.Users.Add(user);
+    // await context.SaveChangesAsync();
+
+    // return new UserDto{
+    //     Username = user.UserName,
+    //     Token = tokenService.CreateToken(user)
+    // };
 }
 
 
